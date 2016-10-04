c963cc9a95caaeca0597e268155d132cb7275175
Anthony Frasso <afrasso@cimpress.com>
2016-10-04 11:41:11 -0400

    Adding tests, changing name and publishing to NPM.

--- src/index.js
+++ src/index.js
@@ -2,44 +2,46 @@ var _       = require("lodash"),
     assert  = require("assert-plus"),
     Promise = require("bluebird");
 
-var RequestPromiseInterceptor = function(rp, interceptors) {
-  if (!(this instanceof RequestPromiseInterceptor)) {
-    return new RequestPromiseInterceptor(rp, interceptors);
+var RequestPromiseMiddlewareFramework = function(rp) {
+  if (!(this instanceof RequestPromiseMiddlewareFramework)) {
+    return new RequestPromiseMiddlewareFramework(rp);
   }
 
   assert.func(rp, "rp");
   this.rp = rp;
 
-  this.initialInterceptor = function(options, callback) {
+  this.initialMiddleware = function(options, callback) {
     rp(_.assign(options, { resolveWithFullResponse: true }))
       .then(response => callback(null, response, response.body))
       .catch(err => callback(err, null, null));
   };
 
-  this.interceptors = [ ];
+  this.middleware = [ ];
 
-  if (interceptors) {
-    this.use(interceptors);
+  if (arguments.length > 1) {
+    var args = _.toArray(arguments);
+    args.shift();
+    _.forEach(args, mw => this.use(mw));
   }
 };
 
-RequestPromiseInterceptor.prototype.use = function(interceptors) {
-  this.interceptors = _.concat(this.interceptors, interceptors);
+RequestPromiseMiddlewareFramework.prototype.use = function(middleware) {
+  this.middleware = _.concat(this.middleware, middleware);
 };
 
-RequestPromiseInterceptor.prototype.getInterceptedRequestPromise = function() {
+RequestPromiseMiddlewareFramework.prototype.getMiddlewareEnabledRequestPromise = function() {
   var me = this;
   var intercept = function(options) {
-    var resolveWithFullResponse = _.at(options, "resolveWithFullResponse");
-    var interceptors = _.concat(me.interceptors, me.initialInterceptor);
+    var resolveWithFullResponse = _.get(options, "resolveWithFullResponse");
+    var middleware = _.concat(me.middleware, me.initialMiddleware);
     var next = function(_options, _callback) {
-      var nextInterceptor = interceptors.shift();
-      nextInterceptor(_options, _callback, next);
+      var nextMiddleware = middleware.shift();
+      nextMiddleware(_options, _callback, next);
     };
     var promisifiedNext = Promise.promisify(next);
     return promisifiedNext(options)
       .then(response => {
-        if (resolveWithFullResponse === true) {
+        if (resolveWithFullResponse) {
           return response;
         }
         return response.body;
@@ -48,4 +50,4 @@ RequestPromiseInterceptor.prototype.getInterceptedRequestPromise = function() {
   return me.rp.defaults(intercept);
 };
 
-module.exports = RequestPromiseInterceptor;
+module.exports = RequestPromiseMiddlewareFramework;
